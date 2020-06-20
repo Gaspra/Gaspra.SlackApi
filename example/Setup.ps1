0f7e60f554b8a18fefa7e91acc47cb10d9dd46c7
Richard Lawrence <richardlaw192@gmail.com>
2020-06-20 16:16:12 +0100

    add setup powershell to example

---
+++ example/Setup.ps1
@@ -0,0 +1,13 @@
+push-location ..
+
+dotnet tool restore
+
+dotnet paket install
+
+dotnet paket restore
+
+dotnet build src/gaspra.slackapi.sln
+
+pop-location
+
+dotnet build gaspra.slackapi.example.sln
