dcb3cae1ea1f0094a9f040190f84d7a81328b4e6
DJamesC <DavidJamesCumming@gmail.com>
2020-09-15 15:40:35 +0100

    Message block buttons

---
+++ src/Gaspra.SlackApi/Models/MessageBlocks/SlackMessageBlockElement.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gaspra.SlackApi.Models.MessageBlocks
+{
+    public class SlackMessageBlockElement
+    {
+        [JsonProperty("type")]
+        protected string Type { get; } = "button";
+
+        [JsonProperty("text")]
+        public SlackMessageBlockTextContent Text { get; set; }
+
+        [JsonProperty("value")]
+        protected string Value { get; set; }
+    }
+}
